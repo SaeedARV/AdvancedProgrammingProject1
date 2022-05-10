5966181a07fbc5a67ff91adeaa4f69d68a584ea3
Moosavi <moosavi238@gmail.com>
2022-05-10 21:28:09 +0430

    create struct article

--- Article.cpp
+++ Article.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 #include "Article.h"
 
-Article::Article(){}
+Article::Article()
+{
+}
 
 Article::~Article() {}
+
+/*
+void User::addArticle(Article *newArticle)
+{
+    this->loginUser->articles.push_back(newArticle);
+}
+
+vector<Article *> User::getArticles()
+{
+    return this->loginUser->articles;
+}
+
+*/
--- Article.h
+++ Article.h
@@ -2,8 +2,17 @@
 #define ARTICLE_H_INCLUDED
 
 #include <bits/stdc++.h>
-//#include "User.h"
+#include "User.h"
 
+struct article
+{
+    std::string name;
+    std::string date;
+    std::string Id;
+    std::string body;
+    std::vector<article *> refId;
+    std::vector<user *> author;
+};
 
 class Article
 {
@@ -11,7 +20,10 @@ private:
 public:
     Article();
     ~Article();
-    };
-
+};
+/*
+void addArticle(Article *newArticle);
+std::vector<Article *> getArticles();
+*/
 
 #endif // ARTICLE_H_INCLUDED
