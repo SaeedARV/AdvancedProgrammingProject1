be971288033f5b3e16c2aee30331ea4566eddde6
Mohammad Saeed Arvenaghi <41886775+SaeedARV@users.noreply.github.com>
2022-05-15 00:11:38 +0430

    added trackArticle function

--- Article.cpp
+++ Article.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 #include "Article.h"
 
+vector<article *> allArticles;
+
 Article::Article(){}
 
 Article::~Article() {}
@@ -11,6 +13,20 @@ void Article::addArticle(article *newArticle)
     if(vArticle(newArticle)){
         for(int i = 0; i < newArticle->authors.size(); i++){
             newArticle->authors[i]->articles.push_back(newArticle);
+            allArticles.push_back(newArticle);
+        }
+    }
+}
+
+void Article::trackArticle(string &id){
+    for(int i = 0; i < allArticles.size(); i++){
+        if(allArticles[i]->id == id){
+            if(vArticle(allArticles[i])){
+                cout << "The article is accepted." << endl;
+            }
+            else{
+                cout << "The article is rejected." << endl;
+            }
         }
     }
 }
